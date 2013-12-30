3d780f389f0356ef653ded46c6ab6cfeec91e5de
Hidenori FUJIMURA <fujimura.hidenori@gmail.com>
2013-12-31 00:15:26 +0900

    update

---
+++ 2013/12/31/AbstractWorker.js
@@ -0,0 +1,25 @@
+L.AbstractWorker = L.Class.extend({
+    initialize: function () {
+    },
+
+    onAdd: function (map) {
+    },
+
+    onRemove: function (map) {
+    },
+
+    process: function(tile, callback) {
+        callback(tile);
+    },
+    
+    abort: function(tile) {
+    },
+    
+    clear: function() {
+    }
+});
+
+// dummy worker (= no worker) when used directly
+L.noWorker = function () {
+    return new L.AbstractWorker();
+};
